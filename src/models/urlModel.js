2030b8763360eecfc67bf7f218c5c086c3828050
Aakash Ghosh <aakash.duke.2019@gmail.com>
2022-10-04 14:20:42 +0530

    ihsf

--- src/models/urlModel.js
+++ src/models/urlModel.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const urlSchema = mongoose.Schema({
 
-    urlCode: { type : String , unique:true , required:true, trim:true }, 
+    urlCode: { type : String ,lowercase:true, unique:true , required:true, trim:true }, 
 
     longUrl: {type:String , required:true }, 
     
