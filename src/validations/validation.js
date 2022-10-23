83074575e4a0dd964110db703af31d9a4aaa896e
Aakash Ghosh <aakash.duke.2019@gmail.com>
2022-10-23 21:33:39 +0530

    a

--- src/validations/validation.js
+++ src/validations/validation.js
@@ -20,15 +20,15 @@ let isValidSize = function (sizes) {
 }
 
 const validPrice = (value) => {
-    return ((/^([0-9]{0,15}((.)[0-9]{0,2}))$/).match(value)
-}
+    return (/^([0-9]{0,15}((.)[0-9]{0,2}))$/).test(value)
+};
 const isvalidpassword = (value) => {
-    return ((/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,15}$/).match(value)
-}
+    return (/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,15}$/).test(value)
+};
 const isvalidemail = (value) => {
-    return ((/^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/).match(value)
+    return (/^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/).test(value)
 }
 const isvalidmobileNumber = (value) => {
-    return ((/^(?:(?:\+|0{0,2})91(\s*[\-]\s*)?|[0]?)?[6789]\d{9}$/).match(value)
+    return (/^(?:(?:\+|0{0,2})91(\s*[\-]\s*)?|[0]?)?[6789]\d{9}$/).test(value)
 }
 module.exports = {isValid, isValidSize, stringChecking, validPrice, isvalidemail, isvalidmobileNumber, isvalidpassword}
